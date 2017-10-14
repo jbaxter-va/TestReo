9e5382a6dbb28192774d09d26b25b32254521cb2
Jonathan Baxter <jbaxter@vendasta.com>
2017-10-14 09:31:16 -0600

    add multiply to main

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import sys
 from fib import fib
 from fib import fibsq
 from fib import fib_times_2
+from math_fun import multiply
 
 print fib(10)
 
 print fibsq(10)
 
 print fib_times_2(10)
+
+print multiply(10,2)
