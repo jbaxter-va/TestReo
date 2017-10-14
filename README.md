282d47e1586ff406f7f147a274b646fc4de2c16a
Jonathan Baxter <jbaxter@vendasta.com>
2017-10-13 19:55:10 -0600

    forgot to add:(

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TestReo
+A repo for me to get some more git practice in
+
--- fib.py
+++ fib.py
@@ -1,4 +1,4 @@
-def fib(x):
+def fib(n):
     fib = [0,1]
     for i in range(2, n+1):
         fib.append(fib[-2]+fib[-1])
